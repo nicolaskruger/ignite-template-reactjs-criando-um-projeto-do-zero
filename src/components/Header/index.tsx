b5e8f9bac7a0d85d2e39fd1012f86d1a0f503a1e
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-27 15:04:25 -0300

    feat: index finish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
-}
+import { FC } from 'react';
+import styles from './header.module.scss';
+
+const Header: FC = () => {
+  return (
+    <header className={styles.container}>
+      <img src="/Logo.svg" alt="" />
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,38 @@
-import { GetStaticProps } from 'next';
-
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
+import { useState } from 'react';
 
+const toPostPaginator = (response: ApiSearchResponse): PostPagination => {
+  const data: Post[] = response.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      'MM LLL yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      author: post.data.author,
+      subtitle: post.data.subtitle,
+      title: post.data.title,
+    },
+  }));
+  return {
+    results: data,
+    next_page: response.next_page,
+  };
+};
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -24,13 +52,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+const Home: NextPage<HomeProps> = ({ postsPagination }) => {
+  // TODO
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const [post, setPost] = useState(postsPagination);
 
-//   // TODO
-// };
+  const handleLoadMore = async () => {
+    const result = await fetch(post.next_page);
+    const data: PostPagination = toPostPaginator(await result.json());
+    setPost({
+      next_page: data.next_page,
+      results: [...post.results, ...data.results],
+    });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Zero Project | Home</title>
+      </Head>
+      <main className={styles.container}>
+        {post.results.map(result => (
+          <button key={result.uid} type="button">
+            <h1>{result.data.title}</h1>
+            <p>{result.data.subtitle}</p>
+            <div>
+              <div>
+                <AiOutlineCalendar />
+                <time>{result.first_publication_date}</time>
+              </div>
+              <div>
+                <AiOutlineUser />
+                <span>{result.data.author}</span>
+              </div>
+            </div>
+          </button>
+        ))}
+        {post.next_page !== null && <button onClick={handleLoadMore} className={styles.loadMore}>Carregar mais posts</button>}
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      fetch: ['posts.title', 'posts.author', 'posts.subtitle'],
+    }
+  );
+
+  // TODO
+
+  return {
+    props: {
+      postsPagination: toPostPaginator(postsResponse),
+    },
+    revalidate: 60 * 30, // 1h
+  };
+};
